2b26a1a216da41549b556a413e8fdf42cf8051cc
LZSopranoS <lz8618857@gmail.com>
2026-08-12 18:55:44 +0800

    Update v6.3

--- LZSHOP/lua/M_LZEXP.lua
+++ LZSHOP/lua/M_LZEXP.lua
@@ -1,35 +1,51 @@
-function LZXPCHAN(slot1,slot2) 
-   local sprite1 = EEex_Sprite_GetInPortrait(slot1)           --第二种写法
-   local currentxp1 = sprite1.m_baseStats.m_xp                --转职后经验会清0，经验只计算新职业的，非原职业+新职业
-   local lzminusxp = math.floor(currentxp1 * 0.1) 
-   C:Eval("AddXPObject(Myself,-"..lzminusxp..")", slot1)
-   C:Eval("CreateVisualEffectObject(\"BDSHSUM\",Myself)", slot1)
-   C:Eval("AddXPObject(Myself,"..lzminusxp..")", slot2)
-   C:Eval("CreateVisualEffectObject(\"BDSHSUM\",Myself)", slot2)
+-- 输入0代表Player1、1代表Player2，和输入0代表队伍头像1不同
+function LZ_GetSpriteByPlayerNum(playerNum)
+	local charactersArray = EngineGlobals.g_pBaldurChitin.m_pObjectGame.m_characters
+	local spriteID = charactersArray:get(playerNum)
+	if not spriteID or spriteID == 0 or spriteID == -1 then return end
+	-- 通过 spriteID 获取 sprite 对象
+	return EEex_GameObject_Get(spriteID)
 end
 
-function LZXPCOP1(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(0).m_baseStats.m_xp
+-- EEex_Sprite_GetInPortrait()改为LZ_GetSpriteByPlayerNum()
+function LZXPCHAN(playerNum1,playerNum2) 
+   local sprite1 = LZ_GetSpriteByPlayerNum(playerNum1)           --第二种写法
+   local currentxp1 = sprite1.m_baseStats.m_xp                   --转职后经验会清0，经验只计算新职业的，非原职业+新职业
+   local lzminusxp = math.floor(currentxp1 * 0.1)
+   local sprite2 = LZ_GetSpriteByPlayerNum(playerNum2)
+   --local portraitNum1 = EEex_Sprite_GetPortraitIndex(sprite1)
+   --local portraitNum2 = EEex_Sprite_GetPortraitIndex(sprite2)
+   local portraitNum1 = sprite1:getPortraitIndex()
+   local portraitNum2 = sprite2:getPortraitIndex()
+   C:Eval("AddXPObject(Myself,-"..lzminusxp..")", portraitNum1)
+   C:Eval("CreateVisualEffectObject(\"BDSHSUM\",Myself)", portraitNum1)
+   C:Eval("AddXPObject(Myself,"..lzminusxp..")", portraitNum2)
+   C:Eval("CreateVisualEffectObject(\"BDSHSUM\",Myself)", portraitNum2)
 end
 
-function LZXPCOP2(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(1).m_baseStats.m_xp
+-- EEex_Sprite_GetInPortrait()改为LZ_GetSpriteByPlayerNum()
+function LZXPCOP1(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(0).m_baseStats.m_xp
 end
 
-function LZXPCOP3(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(2).m_baseStats.m_xp
+function LZXPCOP2(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(1).m_baseStats.m_xp
 end
 
-function LZXPCOP4(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(3).m_baseStats.m_xp
+function LZXPCOP3(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(2).m_baseStats.m_xp
 end
 
-function LZXPCOP5(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(4).m_baseStats.m_xp
+function LZXPCOP4(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(3).m_baseStats.m_xp
 end
 
-function LZXPCOP6(slot) 
-   return EEex_Sprite_GetInPortrait(slot).m_baseStats.m_xp > EEex_Sprite_GetInPortrait(5).m_baseStats.m_xp
+function LZXPCOP5(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(4).m_baseStats.m_xp
+end
+
+function LZXPCOP6(playerNum) 
+   return LZ_GetSpriteByPlayerNum(playerNum).m_baseStats.m_xp > LZ_GetSpriteByPlayerNum(5).m_baseStats.m_xp
 end
 
 --XPGT(LastSummonerOf(Myself),50000)判断遇到转职有问题，转职后判断是原职业+新职业经验之和，原职业经验满足条件转职后用卷轴学特殊能力扣经验会使新职业经验变负，故改成用EEex判断。
